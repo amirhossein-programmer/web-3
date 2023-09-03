a839c9e43c03abe867016b0828f45382e5aaad73
AmirHossein Bakhtiyari <amirhosseinbakhtiyari.program@gmail.com>
2023-09-03 17:17:15 +0330

    I create a token and compile it by hardhat

---
+++ contracts/Token.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0 ;
+
+contract Token {
+    string public name= "HardHat token" ;
+    string public symbol = "HHT";
+    uint public totalSuply = 1000 ;
+
+    address public owner;
+    mapping (address=>uint)balances;
+
+    constructor(){
+        balances[msg.sender] = totalSuply;
+        owner = msg.sender;
+    }
+    function transfer(address to, uint amount) external{
+        require(balances[msg.sender]>=amount, "Not enough funds to transfer");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+}
